8fb646bd6d6c9284da8679e930038d957961e8b1
sandoelio <sandoelio@hotmail.com>
2025-08-28 21:14:18 -0300

    Altera os placeroldes na view de cadastro de atleta

--- resources/views/atletas/create.blade.php
+++ resources/views/atletas/create.blade.php
@@ -33,9 +33,9 @@
                     <input type="file" class="form-control" name="imagem" id="imagem" accept="image/*">
                 </div>
                 <div class="col-md-6 mb-3">
-                    <label for="nome_completo" class="form-label">Nome Completo</label>
+                    <label for="nome_completo" class="form-label">Nome e Sobrenome</label>
                     <input type="text" class="form-control" name="nome_completo" id="nome_completo"
-                        placeholder="Ex: João da Silva" required>
+                        placeholder="Ex: João Silva" required>
                 </div>
             </div>
 
@@ -59,7 +59,7 @@
                 <div class="col-md-6 mb-3">
                     <label for="posicao_jogo" class="form-label">Posição no Jogo</label>
                     <input type="text" class="form-control" name="posicao_jogo" id="posicao_jogo"
-                        placeholder="Ex: Amador, Pivo..." required>
+                        placeholder="Insira somente uma Ex: Amador" required>
                 </div>
             </div>
 
@@ -80,7 +80,7 @@
                 <div class="col-md-6 mb-3">
                     <label for="altura" class="form-label">Altura (m)</label>
                     <input type="number" step="0.01" class="form-control" name="altura" id="altura"
-                        placeholder="Ex: 1.75" required>
+                        placeholder="Ex: 1.75 use o ponto para separar" required>
                 </div>
                 <div class="col-md-6 mb-3">
                     <label for="sexo" class="form-label">Sexo</label>
@@ -102,13 +102,12 @@
 
             <div class="col-md-12 mb-3">
                 <p class="video-instructions">
-                    Para o cadastro do atleta, é necessário que o vídeo esteja hospedado em uma plataforma como YouTube ou
-                    Vimeo.
+                    Para o cadastro do atleta, é necessário que o vídeo esteja hospedado em uma plataforma como ex: YouTube,Instagram.
                     Insira o link do vídeo do atleta demonstrando:
                 </p>
                 <ul class="video-requirements">
-                    <li>03 arremessos do garrafão</li>
-                    <li>03 arremessos da linha de 03 pontos</li>
+                    <li>01 arremessos do garrafão</li>
+                    <li>01 arremessos da linha de 03 pontos</li>
                     <li>01 bandeja do lado esquerdo</li>
                     <li>01 bandeja do lado direito</li>
                     <li>Domínio de bola pelos cones</li>
